6b1ab47c2e9b6a422c6b4cf3f38d811da64264cc
Kalpesh Patel <53889072+quickfinds@users.noreply.github.com>
2019-10-16 22:43:31 +0530

    Update __manifest__.py

--- login_page_purple/__manifest__.py
+++ login_page_purple/__manifest__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Login Page Purple",
-    'summary': """Website portal login page with purple theme and fullscreen""",
+    'summary': """
+    Website portal login page with purple theme and fullscreen""",
     'version': '13.0.1.0.0',
     'category': 'Website',
-    'summary': """Website portal login page with purple theme and fullscreen",
+    'description': """
+    - Website portal login page purple theme
+    - fullscreen background
+    """,
     'author': "Aarav Web Solutions",
     'website': "http://aaravwebsolutions.com",
     'license': 'LGPL-3',
     'images': ['images/loginscreen.png'],
-    'depends': [
-	'website'
-    ],
+    'depends': ['website'],
     'data': [
         'templates/aws_website_login_page.xml',
     ],
