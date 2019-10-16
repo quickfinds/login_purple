4160b3fb5ffa8c0e4e026bbe4e6ad34868cd8505
Kalpesh Patel <53889072+quickfinds@users.noreply.github.com>
2019-10-16 20:51:52 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Login Page Purple',
+    'summary': 'Website portal login page with purple theme and fullscreen',
+    'version': '13.0.1.0.0',
+    'category': 'Website',
+    'summary': 'Website portal login page with purple theme and fullscreen',
+    'author': 'Aarav Web Solutions',
+    'website': 'http://aaravwebsolutions.com',
+    'license': 'LGPL-3',
+    'images': ['images/loginscreen.png'],
+    'depends': [
+	'website',
+    ],
+    'data': [
+        'templates/aws_website_login_page.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
